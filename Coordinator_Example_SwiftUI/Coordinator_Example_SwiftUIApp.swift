3e7815cf115352432fc13fa8177a5012c5bfc51b
teiresias22 <teiresias22@gmail.com>
2022-11-25 14:29:59 +0900

    Initial Commit

---
+++ Coordinator_Example_SwiftUI/Coordinator_Example_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator_Example_SwiftUIApp.swift
+//  Coordinator_Example_SwiftUI
+//
+//  Created by MarkCloud on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Coordinator_Example_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
